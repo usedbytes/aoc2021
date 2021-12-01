7a4f858fcf18a29a79907aa6eb38d93cdfa6b8d9
Brian Starkey <stark3y@gmail.com>
2021-12-01 12:18:03 +0000

    Copy over README and template from last year

---
+++ template.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func doLines(filename string, do func(line string) error) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if err := do(line); err != nil {
+			return err
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func run() error {
+	if err := doLines(os.Args[1], func(line string) error {
+		fmt.Println(line)
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		os.Exit(1)
+	}
+}
