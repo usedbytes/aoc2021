b6b7ad23b70f281fdbb952de4870b6b6d2f66e15
Brian Starkey <stark3y@gmail.com>
2021-12-09 12:32:13 +0000

    09: Part 2, recursive search

--- 09/puzzle.go
+++ 09/puzzle.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sort"
 )
 
 func doLines(filename string, do func(line string) error) error {
@@ -41,8 +42,49 @@ func get(heightMap [][]int, x, y, oob int) int {
 	return heightMap[y][x]
 }
 
-type LowPoint struct {
+func searchAround(heightMap [][]int, pt Point, basin map[Point]bool) {
+	if _, ok := basin[pt]; ok {
+		// Already hit
+		return
+	}
+
+	if get(heightMap, pt.X, pt.Y, 10) >= 9 {
+		// Edge of the basin
+		basin[pt] = false
+		return
+	}
+
+	basin[pt] = true
+
+	dirs := [][2]int {
+		{  0, -1 },
+		{  0,  1 },
+		{ -1,  0 },
+		{  1,  0 },
+	}
+
+	for _, d := range dirs {
+		searchAround(heightMap, Point{pt.X + d[0], pt.Y + d[1]}, basin)
+	}
+}
+
+func basinSize(basin map[Point]bool) int {
+	size := 0
+	for _, v := range basin {
+		if v {
+			size++
+		}
+	}
+
+	return size
+}
+
+type Point struct {
 	X, Y int
+}
+
+type LowPoint struct {
+	Point
 	Height int
 	BasinSize int
 }
@@ -78,8 +120,10 @@ func run() error {
 			   get(heightMap, x, y - 1, 10) > here &&
 			   get(heightMap, x, y + 1, 10) > here {
 				pt := LowPoint{
-					X: x,
-					Y: y,
+					Point: Point {
+						X: x,
+						Y: y,
+					},
 					Height: here,
 				}
 				lowPoints = append(lowPoints, &pt)
@@ -88,11 +132,25 @@ func run() error {
 	}
 
 	part1 := 0
+
+	basinSizes := []int{}
 	for _, p := range lowPoints {
 		part1 += (p.Height + 1)
+
+		basin := make(map[Point]bool)
+		searchAround(heightMap, p.Point, basin)
+		basinSizes = append(basinSizes, basinSize(basin))
 	}
 
-	fmt.Println(part1)
+
+	fmt.Println("Part 1:", part1)
+
+	sort.Ints(basinSizes)
+	part2 := 1
+	for i := len(basinSizes) - 1; i >= len(basinSizes) - 3; i-- {
+		part2 *= basinSizes[i]
+	}
+	fmt.Println("Part 2:", part2)
 
 	return nil
 }
