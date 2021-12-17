740db10ea49d2aa07f8a96d5cb49ec64ce56845b
Brian Starkey <stark3y@gmail.com>
2021-12-17 22:10:09 +0000

    17: Rewrite to just brute-force
    I wasn't very keen on the asymmetric, semi-analytical solution, so just
    use brute-force instead. The range is well-limited.

--- 17/puzzle.go
+++ 17/puzzle.go
@@ -6,6 +6,20 @@ import (
 	"os"
 )
 
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func run() error {
 
 	f, err := os.Open(os.Args[1])
@@ -22,54 +36,24 @@ func run() error {
 	var x1, x2, y1, y2 int
 	fmt.Sscanf(string(bs), "target area: x=%d..%d, y=%d..%d", &x1, &x2, &y1, &y2)
 
-	// Acceleration is -1, so the projectile reaches its peak height
-	// at step 'u', where 'u' is the starting velocity.
-	// From the rules, it will spend 2 'steps' at its peak, one step
-	// with v = 0, and one with v = -1, meaning it returns to y = 0
-	// at step (2 * u) + 1, with a velocity of -(u + 1).
-	//
-	// At this point, we need its velocity to be such that on
-	// _the very next step_, (2 * u) + 2, it lands in the target area.
-	//  * If it lands short of the target area on step (2 * u) + 2,
-	//    then it could have been going faster, and reached a higher
-	//    altitude.
-	//  * If it overshoots the target area, then it was going too
-	//    fast.
-	// To maximise the speed, we need it to land _just inside_ the target
-	// area, nearly overshooting. That means we need the velocity on
-	// step ((2 * u) + 1) (when it reaches y = 0) to be the "lowest"
-	// y position of the target region:
-	//
-	//  -(u + 1) = y1
-	//
-	// So the starting velocity is -(y1 + 1)
-	//
-	// We can simulate the trajectory to find the max altitude.
-	// For some reason, in s = 0.5(u)t, we need to use (t + 1) to get
-	// the right answer. I'm not sure why that is, maybe to do with
-	// the rules of motion we are given?
+	// The Y velocity can't be any more than the furthest edge of the
+	// region, otherwise we'll overshoot on the first step after
+	// leaving (or arriving back at) y = 0. So this sets our Y bounds.
+	maxYVel := max(abs(y1), abs(y2))
 
-	initialVelocity := -(y1 + 1)
-
-	y := 0
-	for v := initialVelocity; v > 0; v-- {
-		y = y + v
-	}
-
-	fmt.Println("Part 1:", y)
-
+	maxYPos := 0
 	hits := make(map[[2]int]bool)
 
-	// For Part 2, we know the velocity has to be between
-	// y1 and +initialVelocity, so just scan
-	// that whole range.
-	//
-	// For each, determine which steps have 'y' in the target
-	// range.
-	for uy := y1; uy <= initialVelocity; uy += 1 {
-		y = 0
+	// Just brute force exhaustive search the whole range
+	for uy := -maxYVel; uy <= maxYVel; uy += 1 {
+		y := 0
+		peak := 0
 		for vy, ty := uy, 0; ; vy, ty = vy - 1, ty + 1 {
 			y = y + vy
+
+			if y > peak {
+				peak = y
+			}
 
 			if y > y2 {
 				// Not there yet
@@ -81,11 +65,25 @@ func run() error {
 				break
 			}
 
-			// I can't figure out how to solve for 'x', so just brute
-			// force it :-)
-			for ux := 0; ux <= x2; ux++ {
+			// This is a valid Y velocity, if it has the highest
+			// peak, save it.
+			// Note: This is expected to happen for uy == maxYVel
+			if peak > maxYPos {
+				maxYPos = peak
+			}
+
+			// Now look for X velocities which will land in the
+			// on the same time step.
+			// The same as for Y, we can't overshoot on our first
+			// step, which sets the upper bound. The problem
+			// description says we can only fire "forward", so 0
+			// is our lower bound.
+			// Note: We could save these results somewhere instead
+			// of re-searching each time.
+			for ux := 0; ux <= max(x1, x2); ux++ {
 				x := 0
 				vx := ux
+				// Simulate trajectory up to 'ty'
 				for tx := 0; tx <= ty; tx++ {
 					x += vx
 					if vx > 0 {
@@ -93,6 +91,7 @@ func run() error {
 					}
 				}
 
+				// And then check if we're inside the target
 				if x >= x1 && x <= x2 {
 					hits[[2]int{ux, uy}] = true
 				}
@@ -100,6 +99,8 @@ func run() error {
 		}
 	}
 
+
+	fmt.Println("Part 1:", maxYPos)
 	fmt.Println("Part 2:", len(hits))
 
 	return nil
