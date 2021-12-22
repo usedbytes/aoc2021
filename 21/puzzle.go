418e4a7f47dc71b6e17479a1f732731fe7ccab29
Brian Starkey <stark3y@gmail.com>
2021-12-22 08:06:51 +0000

    21: Part 2 Approach 2
    Implement "Approach 2" using a map of states instead of the huge "full
    coverage" array.
    
    The map approach is 5-10x slower, but it looks like only ~1/3 states are
    ever reached, so theoretically the map could use less memory.

--- 21/puzzle.go
+++ 21/puzzle.go
@@ -121,6 +121,128 @@ func Part2Approach1(initialPositions [2]int) int64 {
 	}
 }
 
+type State struct {
+	P1Pos, P2Pos     uint8
+	P1Score, P2Score uint8
+}
+
+func MakeState(pos [2]int, score [2]int) State {
+	return State{
+		P1Pos: uint8(pos[0]),
+		P2Pos: uint8(pos[1]),
+		P1Score: uint8(score[0]),
+		P2Score: uint8(score[1]),
+	}
+}
+
+func Part2Approach2(initialPositions [2]int) int64 {
+	// Calculate all the possible scores for the Dirac dice
+	// and the number of ways to reach that score
+	dieScores := make([]int, 10)
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			for k := 1; k <= 3; k++ {
+				dieScores[i + j + k]++
+			}
+		}
+	}
+
+	// Map from State to number of ways to reach that state
+	states := make(map[State]int64)
+
+	// Initial state - one way to reach it
+	states[MakeState(initialPositions, [2]int{0, 0})] = 1
+
+	scores := [2]int{}
+	pos := [2]int{}
+	maxScore := 0
+
+	// Exhaustively populate all possible states
+	// For the rules given, there's ~100k possible states to go through,
+	// and not all of them are reachable, so totally manageable
+	for scores[0] = 0; scores[0] < 21; scores[0]++ {
+		for scores[1] = 0; scores[1] < 21; scores[1]++ {
+			for pos[0] = 1; pos[0] <= 10; pos[0]++ {
+				for pos[1] = 1; pos[1] <= 10; pos[1]++ {
+					n := states[MakeState(pos, scores)]
+					if n == 0 {
+						// No way to reach here, nothing to do
+						continue
+					}
+
+					// Calculate all new states reachable from here, and how many
+					// times they can be reached
+					for move1, ntimes1 := range dieScores {
+						// To start with, assume player 2 doesn't get a turn
+						newPos := [2]int{
+							((pos[0] - 1 + move1) % 10) + 1,
+							pos[1],
+						}
+						newScore := [2]int{
+							scores[0] + newPos[0],
+							scores[1],
+						}
+
+						if newScore[0] > maxScore {
+							maxScore = newScore[0]
+						}
+
+						// If player 2 does get a turn, update player 2, too
+						if newScore[0] < 21 {
+							for move2, ntimes2 := range dieScores {
+								newPos[1] = ((pos[1] - 1 + move2) % 10) + 1
+								newScore[1] = scores[1] + newPos[1]
+
+								if newScore[1] > maxScore {
+									maxScore = newScore[1]
+								}
+
+								states[MakeState(newPos, newScore)] += n * int64(ntimes1) * int64(ntimes2)
+							}
+						} else {
+							states[MakeState(newPos, newScore)] += n * int64(ntimes1)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Now we've exhaustively computed the whole game, figure out how
+	// many times player 1 won first (that is, player 1's score was >= 21,
+	// while player 2's was <= 20
+	p1Wins := int64(0)
+	for p1Score := 21; p1Score <= maxScore; p1Score++ {
+		for p2Score := 20; p2Score >= 0; p2Score-- {
+			for p1Pos := 1; p1Pos <= 10; p1Pos++ {
+				for p2Pos := 1; p2Pos <= 10; p2Pos++ {
+					state := MakeState([2]int{p1Pos, p2Pos}, [2]int{p1Score, p2Score})
+					p1Wins += states[state]
+				}
+			}
+		}
+	}
+
+	// And the same for P2
+	p2Wins := int64(0)
+	for p2Score := 21; p2Score <= maxScore; p2Score++ {
+		for p1Score := 20; p1Score >= 0; p1Score-- {
+			for p1Pos := 1; p1Pos <= 10; p1Pos++ {
+				for p2Pos := 1; p2Pos <= 10; p2Pos++ {
+					state := MakeState([2]int{p1Pos, p2Pos}, [2]int{p1Score, p2Score})
+					p2Wins += states[state]
+				}
+			}
+		}
+	}
+
+	if p1Wins > p2Wins {
+		return p1Wins
+	} else {
+		return p2Wins
+	}
+}
+
 func run() error {
 	var initialPositions [2]int
 	if err := doLines(os.Args[1], func(line string) error {
@@ -168,7 +290,10 @@ func run() error {
 	fmt.Println("Part 1:", scores[(winner + 1) % 2] * nrolls)
 
 	part2 := Part2Approach1(initialPositions)
-	fmt.Println("Part 2:", part2)
+	fmt.Println("Part 2 (Approach 1):", part2)
+
+	part2 = Part2Approach2(initialPositions)
+	fmt.Println("Part 2 (Approach 2):", part2)
 
 	return nil
 }
